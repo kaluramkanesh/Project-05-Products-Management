69a238608a4e17a5d1ed703858fd0fdba881269d
Kaluram <kaluramkanesh123@gmail.com>
2022-07-25 12:44:39 +0530

    userSchema

--- src/Models/userModel.js
+++ src/Models/userModel.js
@@ -30,6 +30,8 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
+        min:8,
+        max:15
     }, // encrypted password
     address: {
         shipping: {
