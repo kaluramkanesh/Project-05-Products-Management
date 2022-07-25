98114ef6607528c7645a1ba7a357e95ae77a8329
dipalidatir-demo <106646972+dipalidatir-demo@users.noreply.github.com>
2022-07-25 17:48:58 +0530

    user login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,56 +1,41 @@
-const userModel = require("../model/userModel")
+const userModel = require("../Models/userModel")
 const mongoose = require('mongoose')
-
-const aws= require("aws-sdk")
-
-aws.config.update({
-    accessKeyId: "AKIAY3L35MCRVFM24Q7U",
-    secretAccessKey: "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",
-    region: "ap-south-1"
-})
+const jwt = require("jsonwebtoken")
 
 
-let uploadFile = async (file) => {
-   return new Promise( function (resolve, reject) {
-    // this function will upload file to aws and return the link
-    let s3= new aws.S3({apiVersion: '2006-03-01'}); // we will be using the s3 service of aws
+//******************user login***********
 
-    var uploadParams= {
-        ACL: "public-read",
-        Bucket: "classroom-training-bucket",  //HERE
-        Key: "abc/" + file.originalname, //HERE 
-        Body: file.buffer
-    }
-
-
-    s3.upload( uploadParams, function (err, data) {
-        if (err) {
-            return reject({"error": err})
-        }
-        // console.log(data)
-        console.log("file uploaded succesfully")
-        return resolve(data.Location)
-    })
-
-    // let data= await s3.upload( uploadParams)
-    // if( data) return data.Location
-    // else return "there is an error"
-
-   })
-}
-
-const createBooks = async function (req, res) {
+const userLogin = async function (req, res) {
     try {
-        let data = req.body
+        let value = req.body
+        let userId = value.email
+        console.log(userId)
+
+        let password = value.password
+
+
+        // *******empty attribute******
+
+        // if (!isValid(userId) || !isValid(password)) return res.status(400).send({ status: false, msg: "Pls Provide  Email And Password both" })
+
+        let user = await userModel.findOne({ $and: [{ email: userId, password: password }] })
+        console.log(user)
+        if (!user) return res.status(400).send({ status: false, msg: "The email or password you are using is wrong" })
+
+        let token = jwt.sign({
+            userId: user._id.toString(),
+            iat: new Date().getTime(),
+            exp: Math.floor(Date.now() / 1000) + 10 * 60 * 60
+        }, "project-5",
         
-        const { fname, lname, email, profileImage, phone, password, address } = data
-
-
-
-
+     )
+     console.log(token)
+        return res.status(200).send({status: true , msg : "succesfully created" , data: token })
     }
-    catch (err) {
-        return res.status(500).send({ status: false, message: err.message })
+
+    catch (error) {
+        return res.status(500).send({ status: false, msg: error.message })
     }
 }
-module.exports.createBooks = createBooks
+
+module.exports.userLogin = userLogin
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,12 +1,16 @@
 const express = require("express")
 // const mongoose=require("mongoose")
-
 const Router = express.Router()
+const userController = require("../controllers/userController")
+// Router.get("/testMe", function (req, res) {
+//     res.status(200).send({ status: true, msg: "Done" })
+// })
 
-Router.get("/testMe", function (req, res) {
-    res.status(200).send({ status: true, msg: "Done" })
-})
 
-Router.post("/")
+
+// *************user login******
+Router.post("/login",userController.userLogin);
 
 module.exports = Router
+
+
